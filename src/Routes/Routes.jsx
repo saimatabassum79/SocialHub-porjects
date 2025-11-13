ee761af700f1ae8a1a8cf434e11bead31a6b81e5
saimatabassum79 <saimatabassum@gmail.com>
2025-11-13 22:02:38 +0200

    update section

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
 import Root from '../components/Outlet/Root/Root';
-import ErrorPage from '../components/Outlet/ErrorPage/Errorpage';
+
 import Home from '../components/Outlet/Home/Home';
 import AppsPage from '../Pages/AppsPage/AppsPage';
 import InstallApp from '../Pages/InstallApp/InstallApp';
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
     {
         path: "/",
         Component: Root,
-        errorElement:<ErrorPage></ErrorPage>,
+        
         children: [
             {
                 index:true  ,
